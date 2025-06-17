ba49dc589cff7222e516ac56dcbb2a9b3b64ffb0
Rafael Bertoldo <chicobertoldo@gmail.com>
2025-06-17 19:59:42 -0300

    refactor: enhance readability and structure

--- cypress/tests/Pages/signUpPage.js
+++ cypress/tests/Pages/signUpPage.js
@@ -28,14 +28,35 @@ class SignUpPage {
     };
   }
 
-  fillPersonalDetails() {
+  registerRandomUser() {
     const user = this.randomUserConstructor();
+    cy.get(this.selectorsList().signUpButton).click();
     cy.get(this.selectorsList().firstName).type(user.firstName);
     cy.get(this.selectorsList().lastName).type(user.lastName);
     cy.get(this.selectorsList().username).type(user.username);
     cy.get(this.selectorsList().password).type("secret3");
     cy.get(this.selectorsList().confirmPassword).type("secret3");
     cy.get(this.selectorsList().submitButton).click();
+
+    cy.get(this.selectorsList().username).type(user.username);
+    cy.get(this.selectorsList().password).type("secret3");
+    cy.get(this.selectorsList().submitButton).click();
+  }
+  registerInvalidUser() {
+    const user = this.randomUserConstructor();
+    cy.get(this.selectorsList().signUpButton).click();
+    cy.get(this.selectorsList().firstName).type(user.firstName);
+    cy.get(this.selectorsList().lastName).type(user.lastName);
+    cy.get(this.selectorsList().username).type(user.username);
+    cy.get(this.selectorsList().password).type("secret3");
+    cy.get(this.selectorsList().confirmPassword).type("secret3");
+    cy.get(this.selectorsList().username).clear();
+    cy.get(this.selectorsList().requiredFieldAlert);
+  }
+
+  registerChecker() {
+    cy.get(this.selectorsList().dialogBox);
+    cy.get(this.selectorsList().closeDialogButton).eq(2).click();
   }
 }
 
--- cypress/tests/e2e/register.spec.js
+++ cypress/tests/e2e/register.spec.js
@@ -1,41 +1,20 @@
 import SignUpPage from "../Pages/signUpPage.js";
 import LoginPage from "../Pages/loginPage";
 
-var Chance = require("chance");
-
-var chance = new Chance();
 const signUpPage = new SignUpPage();
 const loginPage = new LoginPage();
 
 describe("Register a new user", () => {
   it("It should register a new user using valid credentials", () => {
-    const firstName = chance.first();
-    const lastName = chance.last();
-    const username = chance.word();
     loginPage.accessLoginPage();
-    cy.get(signUpPage.selectorsList().signUpButton).click();
-    signUpPage.fillPersonalDetails();
-    cy.get(signUpPage.selectorsList().username).type(username);
-    cy.get(signUpPage.selectorsList().password).type("secret3");
-    cy.get(signUpPage.selectorsList().submitButton).click();
-    cy.get(signUpPage.selectorsList().dialogBox);
-    cy.get(signUpPage.selectorsList().closeDialogButton).eq(2).click();
+    signUpPage.registerRandomUser();
+    signUpPage.registerChecker();
   });
 });
 
-describe.skip("Register a new user with incomplete data", () => {
+describe("Register a new user with incomplete data", () => {
   it("It should assert an error message when registering a new user with missing username", () => {
-    const firstName = chance.first();
-    const lastName = chance.last();
-    const username = chance.word();
-    signUpPage.accessLoginPage();
-    cy.get(signUpPage.selectorsList().signUpButton).click();
-    cy.get(signUpPage.selectorsList().firstName).type(firstName);
-    cy.get(signUpPage.selectorsList().lastName).type(lastName);
-    cy.get(signUpPage.selectorsList().username).type(username);
-    cy.get(signUpPage.selectorsList().username).clear();
-    cy.get(signUpPage.selectorsList().password).type("secret3");
-    cy.get(signUpPage.selectorsList().confirmPassword).type("secret3");
-    cy.get(signUpPage.selectorsList().requiredFieldAlert);
+    loginPage.accessLoginPage();
+    signUpPage.registerInvalidUser();
   });
 });
